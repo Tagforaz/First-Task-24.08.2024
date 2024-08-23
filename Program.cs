688bc7d4ce0eb6b4638778a5fa4e34fca84857ba
Tagforaz <nasibshahverdiyev@gmail.com>
2024-08-24 02:35:56 +0400

    First Task 24.08

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+//    int num = -12;
+//if (num >= 0)
+//{
+//    Console.WriteLine("Musbetdir");
+//}
+//else
+//{
+//    Console.WriteLine("Menfidir");
+//}
+
+
+
+//int num= 24;
+//if(num%7==0 && num%3==0)
+//{
+//    Console.WriteLine("Tam bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Tam bolunmur");
+//}
+
+
+
+int num = 47;
+int onluq = num / 10 ;
+int teklik = num % 10;
+if (onluq > teklik)
+{
+    Console.WriteLine(onluq);
+}
+else 
+{
+    Console.WriteLine(teklik);
+}
+
+
+
